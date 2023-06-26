4b33c2a153772fd40788a089da9088b3e72888ea
clockwo <39350729+clockwo@users.noreply.github.com>
2023-06-26 15:24:20 +0200

    feat: :sparkles: Add subpart (Debugging and Problem Solving)

---
+++ subpart-1/main.js
@@ -0,0 +1,51 @@
+// let userInput = parseInt(prompt('Set value'));
+
+// let fizz = 'Fizz';
+// let buzz = 'Buzz';
+
+// for (let i = 1; i < userInput; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(fizz + buzz);
+//   } else if (i % 3 == 0) {
+//     console.log(fizz);
+//   } else if (i % 5 == 0) {
+//     console.log(buzz);
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+/*
+// Make variable (string) to store input value and variable for password
+let userInput;
+let password = 'qwerty123';
+// Aks user for value and store answer to variable
+userInput = prompt('Write password:');
+// Check if value in variable is equal to password
+if (userInput === password) {
+  // if user input is equal, output positive result
+  console.log('Positive');
+} else {
+  // if user input isn't equal, create while loop (max 3 times) to repeat process again and show message
+  console.log('Wrong password, try again');
+  let counter = 1;
+  while (counter <= 3) {
+    // Aks user for value and store answer to variable again
+    userInput = prompt('Write password again:');
+    // if user input is equal, output positive result and exit loop
+    if (userInput === password) {
+      console.log('Positive');
+      break;
+    } else {
+      console.log('Wrong password, try again');
+    }
+    if (counter == 3) {
+      console.log(
+        'You tried to attempt password to many times, try again after 5 minutes.'
+      );
+      break;
+    }
+    counter++;
+  }
+}
+*/
